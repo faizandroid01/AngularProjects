612d549699adba1b009216aa7880157f71d01e7f
faiz-pitavya <faizrocks9211@gmail.com>
2020-01-08 09:50:34 +0530

    New component is added and registered in App module

--- first-ang-project/src/app/app.module.ts
+++ first-ang-project/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import {ServerComponent} from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ServerComponent
   ],
     imports: [
         BrowserModule,
