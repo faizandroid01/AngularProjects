301a8c70951c3ff7c73f915c1d8928359ab8585f
faiz-pitavya <faizrocks9211@gmail.com>
2020-01-08 23:32:26 +0530

    Creating component through CLI , using component through another component

--- first-ang-project/src/app/app.module.ts
+++ first-ang-project/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import {ServerComponent} from './server/server.component';
+import { BooksComponent } from './books/books.component';
 
 @NgModule({
   declarations: [
-    AppComponent, ServerComponent
+    AppComponent, ServerComponent, BooksComponent
   ],
     imports: [
         BrowserModule,
