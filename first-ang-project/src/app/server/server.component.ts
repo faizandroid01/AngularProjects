12be7a257f40790f90a26fb24cc469208ce79eab
faiz-pitavya <faizrocks9211@gmail.com>
2020-01-22 09:00:08 +0530

    Some Css in main file

--- first-ang-project/src/app/server/server.component.ts
+++ first-ang-project/src/app/server/server.component.ts
@@ -2,8 +2,10 @@ import {Component} from '@angular/core';
 
 @Component({
   // selector: '[app-server]',
+  // tslint:disable-next-line:component-selector
   selector : '.app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
 
