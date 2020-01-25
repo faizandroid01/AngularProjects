1049ffb5bb4772f266f66f9c377446703b65eb2e
faiz-pitavya <faizrocks9211@gmail.com>
2020-01-25 19:31:17 +0530

    Two war binding and  use for binding

--- first-ang-project/src/app/server/server.component.ts
+++ first-ang-project/src/app/server/server.component.ts
@@ -3,10 +3,39 @@ import {Component} from '@angular/core';
 @Component({
   // selector: '[app-server]',
   // tslint:disable-next-line:component-selector
-  selector : '.app-server',
+  selector: '.app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
 
+  allowNewServer = false;
+  serverName = '';
+
+  serverCreationStatus = 'No server Created';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  userName = 'Faiz Ahmed';
+  role = 'Front Office';
+  dept = 10;
+  status = '';
+  through2WayBinding: '';
+
+  getStatus() {
+    return this.status = 'Active';
+  }
+
+  onServerCreated() {
+    this.serverCreationStatus = 'Server Created Successfully ';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (event.target as HTMLInputElement).value;
+  }
 }
+
