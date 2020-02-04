0ddc1fb360d722d5ee1b658c0891718a864d6a23
faiz-pitavya <faizrocks9211@gmail.com>
2020-02-04 09:08:59 +0530

    Thats it for the project till structural directive

--- first-ang-project/src/app/test/test.component.ts
+++ first-ang-project/src/app/test/test.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-test',
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  constructor() { }
+  isToggleOn = false;
+  toggleCount = 1;
+  logs = [];
+
+
+  date: Date = new Date();
+
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
+
+
+  toggleDisplay() {
+    this.logs.push(this.toggleCount++ + '..........' + new Date());
+    this.isToggleOn = !this.isToggleOn;
+
+  }
+
 
 }
