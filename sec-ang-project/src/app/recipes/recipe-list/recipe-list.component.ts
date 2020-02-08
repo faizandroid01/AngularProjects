9c1e1ab135ac06c10dee01b07d6a9e93a7f2f4c2
faiz-pitavya <faizrocks9211@gmail.com>
2020-02-08 21:11:44 +0530

    Basic Structure with All the components andbasic directive ngFor

--- sec-ang-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ sec-ang-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,8 +9,10 @@ import { Recipe } from '../recipes.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('Masala Dosa', 'Spicy South Indian Dish.', 'https://lh3.googleusercontent.com/proxy/O1fPsJgoHt6-KTEozrkRl5B7ioQXYSrYncOUA3ZwycMwJVoVwnrX7PHeouzu2PiIZS_S_KNo4hTfWa5pJWohRwssOstZdzJeW8WPvpG0VzunqWC9X89rJxRW_xQ_F5PmbfyBjzk2Ej3OaEUutXIF8nYYC0YplIaM9rBR7qHRA1bCz_lbfbUU3Hp-ka9iNqq_N9Ju'),
-    new Recipe('Pizza', 'Peppy Paneer \' Dominos', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRI7x6SrbUS4imWE5uM5JdFS5boDkJPUex-nakP3ZkzCbEwXsK5'),
+    new Recipe('Masala Dosa', 'Spicy South Indian Dish.', 'https://lh3.googleusercontent.com/proxy/tH_iag031ey5_j1rjig2BssDRLOf6pE0UegFM_H-tUZAi4XCo-ZdBLxAhjNTzj3oLbl4ivc7yB0PJB1v65ACHHgZsYAkWDhi3xXAEh82OcpjUzBK7bWVjZWbmuIJs-R3EX9UoH3Yamuo65keZEt2Si4lhi-Vc20KZv9dxmLYyxgOkHYEtax-o8ah9lNz6t2kcg8dGJ2AYkkJ'),
+    new Recipe('Pizza', 'Peppy Paneer \' Dominos', 'https://d2mekbzx20fc11.cloudfront.net/uploads/web.Mediterranean-Lamb_2018_600x600.png'),
+    new Recipe('Death By Choclate', 'Polar bear Delicious', 'https://cdn.shopify.com/s/files/1/0918/2274/products/death-by-chocolate-cake-chocolate-cake-ennoble-eat-cake-today-birthday-cake-delivery-klpjmalaysia-583920_grande.jpg?v=1578916850'),
+    new Recipe('Gazar Ka Halwa', 'Delicious Carrot Grind', 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Gajjar_ka_halwa_%28carrot_halwa%29.JPG/1200px-Gajjar_ka_halwa_%28carrot_halwa%29.JPG')
   ]
 
   constructor() { }
